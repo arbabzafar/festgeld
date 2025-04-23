4fd6120f0c04ebe663305bbdb6bd8f033882604f
arbab <arbabzafar3115@gmail.com>
2025-04-23 23:51:50 +0500

    Initial commit from Create Next App

---
+++ app/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>header hm ma</div>
+  )
+}
+
+export default header
---
+++ app/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import header from './components/header'
+
+const page = () => {
+  return (
+    <div>page
+
+      <header />
+    </div>
+  )
+}
+
+export default page
